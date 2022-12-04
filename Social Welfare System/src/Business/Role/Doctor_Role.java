206eae52b67cb2386da2c7a55951f12912be55dc
MakarandZende <zende.m@northeastern.edu>
2022-12-03 23:13:51 -0500

    Added Doctor_Role

---
+++ Social Welfare System/src/Business/Role/Doctor_Role.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.Ecosystem.Ecosystem;
+import Business.Enterprises.Enterprise;
+import Business.Network.NetworkSystem;
+import Business.Organization.Org;
+import Business.UserAccount.User_Account;
+import javax.swing.JPanel;
+import ui.DoctorRole.DoctorAppointmentRequestsJFrame;
+import ui.DoctorRole.DoctorWorkAreaJPanel;
+
+/**
+ *
+ * @author Makarand
+ */
+public class Doctor_Role extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, User_Account account, Org organization, Enterprise enterprise, Ecosystem business, NetworkSystem network) {
+        return new DoctorWorkAreaJPanel(userProcessContainer, account, organization, enterprise, network);
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getName();
+    }
+}
